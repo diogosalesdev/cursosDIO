1419ea397ecfce57ebc91377ec9b63bcef12fdaa
diogosalesdev <diogosalesdev@gmail.com>
2020-06-15 17:12:16 -0300

    All JavaScript basic page finished

---
+++ JavaScript/JS/main.js
@@ -0,0 +1,17 @@
+function clicou() {
+  document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar!</b>";
+}
+
+function redirecionar() {
+  window.open('https:globallabs.academy/');
+}
+
+function trocar() {
+  document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+  //alert('trocar texto');
+}
+
+function voltar() {
+  document.getElementById("mousemove").innerHTML = "Passe o mouse aqui...";
+  //alert('trocar texto');
+}
