8e849d81f3cf8343ba4a97ca699dd23d09967831
diogosalesdev <diogosalesdev@gmail.com>
2020-06-16 22:27:00 -0300

    Food position defined and making the snake eat food ang growing

--- SnakeGame JavaScript/script.js
+++ SnakeGame JavaScript/script.js
@@ -27,7 +27,7 @@ function makeSnake() {
 }
 
 function drawFood() {
-  context.fillStyle = "red";
+  context.fillStyle = "darkgreen";
   context.fillRect(food.x, food.y, box, box);
 }
 
@@ -58,7 +58,12 @@ function startGame() {
   if(direction == "up") snakeY -= box;
   if(direction == "down") snakeY += box;
 
-  snake.pop();
+  if(snakeX != food.x || snakeY != food.y) {
+    snake.pop();
+  } else {
+    food.x = Math.floor(Math.random() * 15 + 1) * box;
+    food.y = Math.floor(Math.random() * 15 + 1) * box;
+  }
 
   let newHead = {
     x: snakeX,
