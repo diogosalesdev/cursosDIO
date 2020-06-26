34ec28c26639962062a8046cabca0d04df92708a
diogosalesdev <diogosalesdev@gmail.com>
2020-06-26 13:15:40 -0300

    Creating a CEP Searcher with Javascript, JQuery and Bootstrap

--- ajax-jquery/JS/main.js
+++ ajax-jquery/JS/main.js
@@ -1,4 +1,6 @@
 function consultaCep() {
+   $(".cep").hide();
+  $(".animeprogressbar").show();
   var cep = document.getElementById("cep").value;
   console.log(cep);
   var url = "https://viacep.com.br/ws/"+cep+"/json/"
@@ -7,12 +9,21 @@ function consultaCep() {
     url: url,
     type: "GET",
     success: function(res) {
+      $(".animeprogressbar").hide();
       console.log(res);
-      document.getElementById("logradouro").innerHTML = res.logradouro;
-      document.getElementById("bairro").innerHTML = res.bairro;
+      $("#logradouro").html(res.logradouro); //JQuery method
+      $("#bairro").html(res.bairro); //JQuery method
       document.getElementById("cepibge").innerHTML = res.cep;
       document.getElementById("localidade").innerHTML = res.localidade;
       document.getElementById("uf").innerHTML = res.uf;
-    }
+      $(".cep").show();
+    }    
   })
 }
+
+//Using JQuery
+$(function() {
+  $(".cep").hide();
+  $(".animeprogressbar").hide();
+});
+
